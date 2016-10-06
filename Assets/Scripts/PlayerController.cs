1fd47d72208fb7e5cf8d36bfb39b4cbeabca4090
Vamsi Rageer <vamsair@gmail.com>
2016-10-06 17:58:45 -0400

    Unity Roll a ball tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Text scoreText;
+	public Text winText;
+
+	private Rigidbody rigidBody;
+	private int count= 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+		SetScoreText ();
+		winText.text = "";
+	}
+	
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce (movement* speed);
+	
+	}
+
+	void SetScoreText()
+	{
+		scoreText.text = "Score: " + count.ToString();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count +1;
+			SetScoreText();
+
+			if(count >=12)
+			{
+				winText.text = "Hurray!!! You Win!";
+			}
+		}
+	}
+}
